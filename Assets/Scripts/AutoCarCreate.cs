5b189487bec48ddc5cb5b1fda10e615fc5820454
Nikita Maslov <mni@pkzdrav.ru>
2023-12-27 19:30:42 +0300

    feat: NavMesh-Ground.asset correction, added spawn enemy cars

--- Assets/Scripts/AutoCarCreate.cs
+++ Assets/Scripts/AutoCarCreate.cs
@@ -19,10 +19,10 @@ public class AutoCarCreate : MonoBehaviour
         {
             yield return new WaitForSeconds(time);
             Vector3 pos = new Vector3(
-                transform.GetChild(0).position.x+UnityEngine.Random.Range	(3f,7f),
-                  transform.GetChild(0).position.y,
-                transform.GetChild(0).position.z+UnityEngine.Random.Range	(3f,7f),
-                )
+                transform.GetChild(0).position.x + UnityEngine.Random.Range(3f, 7f),
+                transform.GetChild(0).position.y,
+                transform.GetChild(0).position.z + UnityEngine.Random.Range(3f, 7f)
+            );
             Instantiate(car, pos, Quaternion.identity);
         }
     }
---
+++ Assets/Scripts/EnemyGenerate.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] points;
+    public GameObject factory;
+
+    public void Start()
+    {
+        StartCoroutine(SpawnFactory());
+    }
+
+    IEnumerator SpawnFactory()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            yield return new WaitForSeconds(10f);
+            GameObject spawned = Instantiate(factory);
+            Destroy(spawned.GetComponent<PlaceObjects>());
+            spawned.transform.position = points[i].position;
+            spawned.transform.rotation = Quaternion.Euler(
+                new Vector3(0, UnityEngine.Random.Range(0, 360), 0)
+                );
+            spawned.GetComponent<AutoCarCreate>().enabled = true;
+        }
+    }
+}
